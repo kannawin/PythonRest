86b5c19e9e7201d06cd8b46bcc198312294b8162
kannawin <scott.kannawin@gmail.com>
2018-07-13 14:49:05 -0500

    dataframe init

--- quandl.py
+++ quandl.py
@@ -1,8 +1,14 @@
 import quandl
+import pandas
+import numpy
 
 api_key = 'LrpdsDS_QoC_2z_BbHoS'
 reference = 'CHRIS/MGEX_IH1'
 
+##sets you API KEY
 quandl.ApiConfig.api_key = api_key
 
-quandl.get(reference)
+##quandl.get retrieves the data, it already comes in the form of a pandas dataframe
+df = pandas.DataFrame(quandl.get(reference))
+
+##Do what you need here with the data frame
